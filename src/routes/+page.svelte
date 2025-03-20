6d18c9344b999a577aff3ad568c3cc729964fd78
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-03-20 13:00:30 -0300

    aula

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,4 @@
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum adipisci laudantium amet molestiae placeat, natus dolor explicabo. Pariatur, distinctio corrupti eligendi enim cumque placeat soluta voluptatibus mollitia maxime, nulla quam.</p>
 
-<img src="images/my_image.jpeg" width="500" height="500" alt="A dog">
+<img src="images/my_image.jpeg" width="500" height="500" alt="A DOG">
