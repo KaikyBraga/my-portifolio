5d2bbddb9dc10e9ca0fc6669e407c4537ca928c9
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-03-20 12:53:37 -0300

    aula 20-03

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Kaiky Braga</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum adipisci laudantium amet molestiae placeat, natus dolor explicabo. Pariatur, distinctio corrupti eligendi enim cumque placeat soluta voluptatibus mollitia maxime, nulla quam.</p>
+
+<img src="images/my_image.jpeg" width="500" height="500" alt="A dog">
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,12 @@
+<form action="mailto:foo@example.com" method="get" enctype="text/plain">
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email" placeholder="your@email.com" required><br><br>
+
+  <label for="subject">Subject:</label><br>
+  <input type="text" id="subject" name="subject" placeholder="Subject" required><br><br>
+
+  <label for="body">Message:</label><br>
+  <textarea id="body" name="body" placeholder="Your Message" rows="5" required></textarea><br><br>
+
+  <button type="submit">Send</button>
+</form>
